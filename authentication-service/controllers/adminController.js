57392c0a41748ebeaffecfa1f604255d6250a72f
YashikaDesul <yashikadesul07@gmail.com>
2025-02-16 19:21:58 -0500

    Added authentication-service as branch with file contents

---
+++ authentication-service/controllers/adminController.js
@@ -0,0 +1,26 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+//API from Admin Login
+
+const loginAdmin = async (req,res) => {
+    try {
+        const {email,password} = req.body
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+
+        } else {
+            res.json({success:false,message:"Invalid credentials"})
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+    
+}
+
+export {loginAdmin}
