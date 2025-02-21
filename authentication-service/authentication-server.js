f5646c4832f869ffcfa550f4a83357cc0b852d89
RupeshVanneldas <rvanneldas2000@gmail.com>
2025-02-21 02:53:34 -0500

    CORS Orging all

--- authentication-service/authentication-server.js
+++ authentication-service/authentication-server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 // api endpoints
 app.use("/api/admin", adminRouter);
